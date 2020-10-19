73d90333485f9feaf7fd0f574146459993b38a74
Michael Mok <pmmmwh@gmail.com>
2020-10-19 14:42:30 +0200

    fix: block progress during Xcode cli installation

--- _lib/@macos/ensure_brew.zsh
+++ _lib/@macos/ensure_brew.zsh
@@ -1,7 +1,15 @@
 ensure_brew() {
+  installed_xcode_cli_tools() {
+    xcode-select --print-path &>/dev/null
+  }
+
   # Check for Xcode CLI tools
   logger "info" "Checking for Xcode CLI tools ..."
-  sudo xcode-select --install 2>/dev/null
+  xcode-select --install 2>/dev/null
+
+  until installed_xcode_cli_tools; do
+    sleep 5
+  done
 
   # Check for Homebrew, and install it if not found
   logger "info" "Checking for Homebrew installation ..."
@@ -9,7 +17,7 @@ ensure_brew() {
   if (( ! ${+commands[brew]} )); then
     logger "warning" "Homebrew not found, downloading from source ..."
 
-    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
     logger "success" "Successfully installed Homebrew."
   fi
